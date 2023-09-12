d93684578c48c096549bcb15903fbbb98a8836ba
Alper <alpersarginercun@gmail.com>
2023-09-12 12:19:20 +0300

    uzmanlık alanları sayfası yenilendi

--- app/src/main/java/com/example/myapplication/Alanlar.java
+++ app/src/main/java/com/example/myapplication/Alanlar.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,12 +10,21 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class Alanlar extends AppCompatActivity {
 
     private ListView listView;
     private Button btnMenu;
+    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,56 +34,29 @@ public class Alanlar extends AppCompatActivity {
         listView = findViewById(R.id.alanListesi);
         btnMenu = findViewById(R.id.anaMenu);
 
-        ArrayList<String> alanlar = new ArrayList<>();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("alanlar");
+
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter(Alanlar.this, android.R.layout.simple_list_item_1, list);
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    String value = postSnapshot.getValue(String.class).toString();
+                    list.add(value);
+                    listView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
-        alanlar.add("Acil Tıp");
-        alanlar.add("Adli Tıp");
-        alanlar.add("Aile Hekimliği");
-        alanlar.add("Anatomi");
-        alanlar.add("Anesteziyoloji ve Reanimasyon");
-        alanlar.add("Askeri Sağlık Hizmetleri");
-        alanlar.add("Beyin ve Sinir Cerrahisi");
-        alanlar.add("Çocuk Cerrahisi");
-        alanlar.add("Çocuk Sağlığı ve Hastalıkları");
-        alanlar.add("Çocuk ve Ergen Ruh Sağlığı ve Hastalıkları");
-        alanlar.add("Deri ve Zührevi Hastalıkları");
-        alanlar.add("Enfeksiyon Hastalıkları ve Klinik Mikrobiyoloji");
-        alanlar.add("Fiziksel Tıp ve Rehabilitasyon");
-        alanlar.add("Fizyoloji");
-        alanlar.add("Genel Cerrahi");
-        alanlar.add("Göğüs Cerrahisi");
-        alanlar.add("Göğüs Hastalıkları");
-        alanlar.add("Göz Hastalıkları");
-        alanlar.add("Halk Sağlığı");
-        alanlar.add("Hava ve Uzay Hekimliği");
-        alanlar.add("Histoloji ve Embriyoloji");
-        alanlar.add("İç Hastalıkları");
-        alanlar.add("Kadın Hastalıkları ve Doğum");
-        alanlar.add("Kalp ve Damar Cerrahisi");
-        alanlar.add("Kardiyoloji");
-        alanlar.add("Kulak Burun Boğaz Hastalıkları");
-        alanlar.add("Nöroloji");
-        alanlar.add("Nükleer Tıp");
-        alanlar.add("Ortopedi ve Travmatoloji");
-        alanlar.add("Plastik, Rekonstrüktif ve Estetik Cerrahi");
-        alanlar.add("Radyasyon Onkolojisi");
-        alanlar.add("Radyoloji");
-        alanlar.add("Ruh Sağlığı ve Hastalıkları");
-        alanlar.add("Spor Hekimliği");
-        alanlar.add("Sualtı Hekimliği ve Hiperbarik Tıp");
-        alanlar.add("Tıbbi Biyokimya");
-        alanlar.add("Tıbbi Ekoloji ve Hidroklimatoloji");
-        alanlar.add("Tıbbi Farmakoloji");
-        alanlar.add("Tıbbi Genetik");
-        alanlar.add("Tıbbi Mikrobiyoloji");
-        alanlar.add("Tıbbi Patoloji");
-        alanlar.add("Üroloji");
-
-
-        ArrayAdapter<String> alanAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alanlar);
-
-        listView.setAdapter(alanAdapter);
 
         btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -97,7 +97,7 @@ public class Login extends AppCompatActivity {
 
                                 }
                                 else {
-                                    Toast.makeText(Login.this, "Doğrulama Başarısız", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(Login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
